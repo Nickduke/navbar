8c16f9587d70afc93b99ca039c6350c71184b2cb
Nick <ntoukas.nick@gmail.com>
2022-11-13 12:09:32 -0500

    logic complete, project complete

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+// 1st method
+// navToggle.addEventListener('click', () => {
+//   if (links.classList.contains('show-links')) {
+//     links.classList.remove('show-links');
+//   } else {
+//     links.classList.add('show-links');
+//   }
+// });
+
+// Best method
+navToggle.addEventListener('click', () => {
+  links.classList.toggle('show-links');
+});
